21ac3c1cd012f2544aca285b8ee2999f1b6d3cb1
Liran Tal <liran.tal@gmail.com>
2014-10-11 19:38:46 +0300

    daloRADIUS website - static files hosting

---
+++ index.php
@@ -0,0 +1,288 @@
+<?php
+    include('common.php');
+?>
+
+<!doctype html>
+<html lang="cs">
+    <head>
+        <meta charset="utf-8">
+        <title>daloRADIUS</title>
+        <meta name="description" content="">
+        <meta name="keywords" content="radius daloradius hotspot wi-fi isp freeradius aaa authentication">
+        <meta name="author" content="David Kohout & Tomáš Lach - www.tomaslach.cz">
+        <link rel="shortcut icon" href="/favicon.ico">
+        <link rel="apple-touch-icon" href="/apple-touch-icon.png">
+        <link rel="stylesheet" href="css/main.css?v=1.0" media="screen,projection">
+        <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.6.4/jquery.min.js"></script>
+        <script src="js/jquery.mousewheel-3.0.4.pack.js"></script>
+        <script src="js/jquery.fancybox-1.3.4.pack.js"></script>
+        <script src="js/custom.js"></script>
+        <!--[if lt IE 9]>
+        <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+        <![endif]-->
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10948308-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
+</script>
+
+    </head>
+    <body>            
+
+        <div id="wrap">
+            <header>            
+                <div id="header">   
+
+                    <a id="logo" href="./" title="daloRADIUS">daloRADIUS</a>
+
+                    <hr> 
+
+                    <nav>
+                        <ul>
+                            <li class="active"><a href="index.php" title="Home">Home</a></li>
+                            <li><a href="testimonials.php" title="Community">Community</a></li>
+                        </ul>          
+                    </nav>
+                    
+                    <hr>
+                    
+                    <p id="icons">
+                        <a href="https://sourceforge.net/projects/daloradius" class="sourceforge">SourceForge</a>
+                    </p>
+
+                </div> <!-- / #header -->
+
+                <hr>
+
+                <div id="intro">
+                    
+                    <div id="slider">
+                        <ul>
+                            <li>
+                                <div class="padding">
+                                    <h1>Easy UI Management</h1>
+                                    <p>
+                                        No more mysql database console.<br/>
+                                        No more editing freeradius configuration files.<br/>
+                                        <br/>
+                                        with daloRADIUS you can easily and quickly manage your FreeRADIUS deployment, thanks to flexible user inteface and navigation flow.
+                                    </p>
+
+                                    <!-- <a href="./" class="button" title="Learn more"><span>Learn more</span></a> -->
+                                </div>
+                                
+                                <div id="image">
+                                    <img src="./tmp/mng1_1.png" alt="Screen">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="padding">
+                                    <h2>Business Intelligence</h2>
+                                    <p>
+                                        By providing you with smart graphical reporting capabilities, daloRAIDUS empowers you to make smart decisions based on actual business insight.
+                                    </p>
+
+                                    <!-- <a href="./" class="button" title="Learn more"><span>Learn more</span></a> -->
+                                </div>
+                                
+                                <div id="image">
+                                    <img src="./tmp/graph2_1.png" alt="Screen">
+                                </div>
+                            </li>
+                            <li>
+                                <div class="padding">
+                                    <h2>Accounting Records</h2>
+                                    <p>
+                                        + Easily manage your user's accounting records. <br/>
+                                        + Quickly find users based on numerous connection attributes. <br/>
+                                        <br/>
+
+                                        Browsing through your accounting records has never been <br/> easier.
+                                    </p>
+
+
+                                    <!-- <a href="./" class="button" title="Learn more"><span>Learn more</span></a> -->
+                                </div>
+                                
+                                <div id="image">
+                                    <img src="./tmp/accounting1_2.png" alt="Screen">
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                    
+                    <a href="./" class="prev" title="Prev">Prev</a>
+                    <a href="./" class="next" title="Next">Next</a>
+
+                </div> <!-- / #intro -->
+
+                <hr>
+
+                <div id="subIntro">
+                    <p>
+                        <i>
+                        <?php echo $testimonials[array_rand($testimonials)]; ?>
+                        </i>
+                        <a href="testimonials.php" class="more" title="Learn more">Learn more ›</a>
+                    </p>                    
+                </div> <!-- / #subIntro -->
+                
+            </header>
+
+            <hr>
+
+            <div id="container">           
+
+                <div class="cols clearFix">
+
+                    <div class="col1-3 withIcon icon1">
+                        <h2>The Book</h2>
+                        <p>
+                        Spanning roughly 250 pages with step-by-step screenshots-based instructions, this book will guide you on managing a daloRADIUS deployed server to run FreeRADIUS AAA system.
+                        <br/>
+                        <a href="http://www.amazon.com/daloRADIUS-User-Guide-Volume-1/dp/1463752199" class="button" title="Learn more"><span>Get the book!</span></a>
+                        </p>
+                    </div>
+
+                    <div class="col1-3 withIcon icon2">
+                        <h2>Virtual Machine Appliance</h2>
+                        <p>daloRADIUS appliance comes pre-installed with dalo, freeradius v2 and the LAMP stack to get you up and running a hotspot or basic authentication service in no-time.
+                        <br/>
+                        <a href="https://sourceforge.net/projects/daloradius/files/daloradius/daloRADIUS%20VM/" class="button" title="Learn more"><span>Get the appliance!</span></a>
+                        </p>
+                    </div>
+                    
+                    <div class="col1-3">
+                    <script type="text/javascript"><!--
+google_ad_client = "ca-pub-9677997565991773";
+/* daloRADIUS website */
+google_ad_slot = "6532548163";
+google_ad_width = 200;
+google_ad_height = 200;
+//-->
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+
+</div>
+<!--
+                    <div class="col1-3 withIcon icon3">
+                        <h2>Camera</h2>
+                        <p>
+                        <script type="text/javascript" src="http://www.ohloh.net/p/daloradius/widgets/project_partner_badge.js"></script>
+                        </p>
+                    </div>
+-->
+                </div> <!-- / .cols -->                                
+
+                <hr> 
+
+<br/><br/>
+
+                <div id="content" class="clearFix">
+
+                    <div class="clearFix">
+                        
+                        <div class="col1-2">
+
+                            <h2>About daloRADIUS</h2>
+
+                            <p class="bold">
+daloRADIUS is an advanced RADIUS web platform aimed at managing Hotspots and general-purpose ISP deployments. It features rich user management, graphical reporting, accounting, and integrates with GoogleMaps for geo-locating (GIS). 
+daloRADIUS is written in PHP and JavaScript and utilizes a database abstraction layer which means that it supports many database systems, among them the popular MySQL, PostgreSQL, Sqlite, MsSQL, and many others.
+</p>
+
+                            <p>
+It is based on a FreeRADIUS deployment with a database server serving as the backend. Among other features it implements ACLs, GoogleMaps integration for locating hotspots/access points visually and many more features.
+
+daloRADIUS is essentially a web application to manage a radius server so theoretically it can manage any radius server but specifically it manages FreeRADIUS and it's database structure. Since version 0.9-3 daloRADIUS has introduced an application-wide database abstraction layer based on PHP's PEAR::DB package which support a range of database servers.
+</p>
+
+                        </div> <!-- / .col1-2 -->
+
+                        <div class="col1-2">
+
+<script type="text/javascript" src="http://www.ohloh.net/p/daloradius/widgets/project_basic_stats.js"></script>
+<br/><br/>
+
+<script type="text/javascript" src="http://www.ohloh.net/p/daloradius/widgets/project_languages.js"></script>
+
+
+<!--
+                            <h2>Recent projects</h2>
+
+                            <p class="gallery clearFix">
+                                <a class="photo" href="./tmp/image1.png" rel="gallery"><img src="./tmp/image1.png" alt="empty"></a>
+                                <a class="photo" href="./tmp/image2.png" rel="gallery"><img src="./tmp/image2.png" alt="empty"></a>
+                                <a class="photo" href="./tmp/image3.png" rel="gallery"><img src="./tmp/image3.png" alt="empty"></a>
+                                <a class="photo" href="./tmp/image4.png" rel="gallery"><img src="./tmp/image4.png" alt="empty"></a>
+                            </p>
+
+                            <h4>Spotify</h4>
+
+                            <p>Spotify is a digital music service that gives you access to millions of songs. Lorem ipsum dolor sit amet consectetur adipisicing.</p>
+
+                            <a href='/' class="more" title="View project">View project ›</a>
+-->
+                        </div> <!-- / .col1-2 -->
+                    
+                    </div> <!-- / .clearFix -->
+                    
+                    <hr>
+<!--
+                    <div id="twitterBar">
+                        <div class="wrap">
+                            <span class="icon"></span>
+                            <p>Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. <a href="./">http://www.google.com</a> <small>2 hours ago</small></p>
+                        </div>
+                    </div>
+-->                    
+                </div> <!-- / #content -->
+                   
+            </div> <!-- / #content -->        
+            
+            <hr> 
+            
+            <footer>
+                <div id="footer" class="clearFix">
+                        
+                    <p class="floatLeft">
+                        &copy; 2007 &ndash; <a href="http://www.enginx.com/blog">Liran Tal</a>, All rights reserved | 
+                        an <a href="http://www.enginx.com">enginx</a> product
+                    </p>
+                    
+                    <!--
+                    <p class="floatRight">
+                        <a href="./" class="contact">Contact us</a>
+                        <a href="./" class="twitter">Follow us on Twitter</a>
+                    </p>
+                    -->
+
+            </footer>
+            
+            <script type="text/javascript">
+google_ad_client = "ca-pub-9677997565991773";
+/* daloRADIUS site 2 */
+google_ad_slot = "8346188197";
+google_ad_width = 728;
+google_ad_height = 90;
+</script>
+<script type="text/javascript"
+src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+</script>
+            
+        </div> <!-- / #wrap -->
+        
+    </body>
+</html>
+
