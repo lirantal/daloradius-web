699141a84d51a5a5880be2c2826e968b30560ba4
Liran Tal <liran.tal@gmail.com>
2014-10-12 11:45:38 +0300

    cleaned up javascript and moved it to the bottom

--- index.php
+++ index.php
@@ -21,20 +21,6 @@
         <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
         <![endif]-->
 
-<script type="text/javascript">
-
-  var _gaq = _gaq || [];
-  _gaq.push(['_setAccount', 'UA-10948308-2']);
-  _gaq.push(['_trackPageview']);
-
-  (function() {
-    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
-    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
-    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
-  })();
-
-</script>
-
     </head>
     <body>            
 
@@ -270,16 +256,30 @@ daloRADIUS is essentially a web application to manage a radius server so theoret
 
             </footer>
             
-            <script type="text/javascript">
-google_ad_client = "ca-pub-9677997565991773";
-/* daloRADIUS site 2 */
-google_ad_slot = "8346188197";
-google_ad_width = 728;
-google_ad_height = 90;
+
+<script type="text/javascript">
+
+  var _gaq = _gaq || [];
+  _gaq.push(['_setAccount', 'UA-10948308-2']);
+  _gaq.push(['_trackPageview']);
+
+  (function() {
+    var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+    ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+    var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+  })();
+
 </script>
-<script type="text/javascript"
-src="http://pagead2.googlesyndication.com/pagead/show_ads.js">
+
+<script type="text/javascript">
+    google_ad_client = "ca-pub-9677997565991773";
+    /* daloRADIUS site 2 */
+    google_ad_slot = "8346188197";
+    google_ad_width = 728;
+    google_ad_height = 90;
 </script>
+
+<script type="text/javascript" src="http://pagead2.googlesyndication.com/pagead/show_ads.js"> </script>
             
         </div> <!-- / #wrap -->
         
